bafc51f2591b5f4e33db6345b7acd2403c38ce6f
briiiqtt <briiiqtt>
2022-04-04 13:25:21 +0900

    initial

---
+++ projectbsc/src/main/java/com/jamong/projectbsc/ProjectbscApplication.java
@@ -0,0 +1,14 @@
+package com.jamong.projectbsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class ProjectbscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbscApplication.class, args);
+	}
+
+}
---
+++ projectbsc/src/main/java/com/jamong/projectbsc/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.jamong.projectbsc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+}
